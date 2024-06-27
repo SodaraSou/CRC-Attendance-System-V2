0f5c51c9344a734656bdcc52aed3d6464bed1966
Sodara Sou <121216473+SodaraSou@users.noreply.github.com>
2024-06-27 09:37:18 +0700

    added turso db

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { postsTable, usersTable } from "@/db/schema";
 
-export default function Home() {
+export default async function Home() {
+  const post = await db.query.postsTable.findMany();
   return (
     <main>
-      <Button>Hello</Button>
+      <form
+        action={async () => {
+          "use server";
+          await db.insert(postsTable).values({
+            title: "Hello",
+            content: "dara",
+            userId: 1,
+          });
+        }}
+      >
+        <Button>Submit</Button>
+      </form>
+      {post.map((post) => (
+        <>{post.title}</>
+      ))}
     </main>
   );
 }
